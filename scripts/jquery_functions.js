38ee1742a36fd0391f47f5f1eb8043c726c65157
Ponyo2311 <dominika.guttenova@gmail.com>
2023-05-07 20:03:04 +1000

    new

--- scripts/jquery_functions.js
+++ scripts/jquery_functions.js
@@ -1,4 +1,13 @@
+// closed or open state of the navigation bar
 let state = 'closed';
+let contactState ='closed';
+
+// // variables to exclude from closing menu trigger list
+// let navigation = document.getElementById("navigation");
+// let menu2hide = document.getElementById ("menu2hide");
+// let contactButton = document.getElementById ("contactButton");
+// // let menuButton = document.getElementById("menuButton");
+
 
 $(document).ready(function(){ //This is to prevent any jQuery code from running before the document is finished loading (is ready).
     $(".menuButton").click(function(){
@@ -10,10 +19,33 @@ $(document).ready(function(){ //This is to prevent any jQuery code from running 
             state = 'closed';
         }
     });
+
+    $("#contactButton").click(function(){
+        if (contactState == 'closed'){
+            $("#contactEmail").slideDown();
+            contactState = 'open';
+        }else{
+            $("#contactEmail").slideUp();
+            contactState = 'closed';
+        }
+    });
+
 });
 
 document.onclick = function(e){
-    if(e.target.id !== 'navigation' && e.target.id !== 'menu2hide' && e.target.className !== 'brick'){
+    // console.log(e.target.className)
+    // if(e.target.id == "contactButton" || e.target.parentNode.id =="contactButton"){
+    
+    //     $('nav').hide();
+    //     state = 'closed';
+    // }
+    if(e.target.id !== 'contactButton' && e.target.id !== 'navigation' && e.target.id !== 'menu2hide' && e.target.className !== 'brick' && e.target.id !== "contactButton" && e.target.parentNode.id !=="contactButton" ){
+        
+        if (contactState == 'open'){
+            $("#contactEmail").hide();
+            contactState = 'closed';
+        }
+
         $('nav').hide();
         state = 'closed';
     }
