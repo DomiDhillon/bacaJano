54022a5b72c3914888187dd75817fee4733a11cd
Ponyo2311 <dominika.guttenova@gmail.com>
2023-05-30 20:15:53 +1000

    commit all

--- scripts/jquery_functions.js
+++ scripts/jquery_functions.js
@@ -14,9 +14,17 @@ $(document).ready(function(){ //This is to prevent any jQuery code from running 
 });
 
 document.onclick = function(e){
+    // console.log(e.target);
     if(e.target.id !== 'navigation' && e.target.id !== 'menu2hide' && e.target.className !== 'brick'){
         
         $('nav').hide();
         state = 'closed';
     }
+    if(e.target.id == 'sheep' || e.target.parentNode.id == "sheep"){
+        window.open('./visite.html');
+    }
+    if(e.target.className == 'closeButton' || e.target.parentNode.className == "closeButton"){
+        window.close();
+    }
 };
+
